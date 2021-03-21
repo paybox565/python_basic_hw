bc1a9649133dbbbbc46bbada260f8d972b9f65e4
Vygovskii Oleg <vygovskii@yandex.ru>
2021-03-21 15:55:33 +0300

    HW01 Fix: even/odd. add github tests

--- homework_01/main.py
+++ homework_01/main.py
@@ -23,10 +23,14 @@ PRIME = "prime"
 
 
 def is_prime(n):
-    if n == 2 or n == 3: return True
-    if n < 2 or n % 2 == 0: return False
-    if n < 9: return True
-    if n % 3 == 0: return False
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
     r = int(n ** 0.5)
     # since all primes > 3 are of the form 6n ± 1
     # start with f=5 (which is prime)
@@ -35,8 +39,10 @@ def is_prime(n):
     f = 5
     while f <= r:
         print('\t', f)
-        if n % f == 0: return False
-        if n % (f + 2) == 0: return False
+        if n % f == 0:
+            return False
+        if n % (f + 2) == 0:
+            return False
         f += 6
     return True
 
@@ -50,23 +56,15 @@ def filter_numbers(numbers, filter_type):
     result_numbers = []
     if filter_type == "odd":
         for number in numbers:
-            if number % 2 == 0:
+            if number % 2 != 0:
                 result_numbers.append(number)
     elif filter_type == "even":
         for number in numbers:
-            if number % 2 != 0:
+            if number % 2 == 0:
                 result_numbers.append(number)
     elif filter_type == "prime":
         for number in numbers:
             if is_prime(number):
                 result_numbers.append(number)
-    else:
-        return "Unknown type"
 
     return result_numbers
-
-
-list_example = (range(1, 30))
-# result = power_numbers(list_example)
-result = filter_numbers(list_example, PRIME)
-print(result)
